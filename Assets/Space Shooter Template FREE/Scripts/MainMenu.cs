cec3b9b144bc18bdab74b6f11a5ee0cd872e92b4
dangtiendat24 <dtdatwork01@example.com>
2026-02-28 21:26:11 +0700

    add demo5

---
+++ Assets/Space Shooter Template FREE/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // BẮT BUỘC: Gọi thư viện này ra thì mới chuyển cảnh được
+
+public class MainMenu : MonoBehaviour
+{
+    // Hàm này sẽ được kích hoạt khi người chơi click vào nút Play
+    public void OnPlayButtonClicked()
+    {
+        // Yêu cầu Unity tải Scene có tên là "Battle"
+        SceneManager.LoadScene("Battle");
+    }
+}
